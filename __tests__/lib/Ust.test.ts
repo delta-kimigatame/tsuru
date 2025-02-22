62e0169ba75299a59e30247ed74ba29131f588c2
delta_kimigatame <sankaku.kuro@gmail.com>
2025-02-23 07:10:18 +0900

    Ust読み込み処理とそのtestingの作成

---
+++ __tests__/lib/Ust.test.ts
@@ -0,0 +1,243 @@
+import * as iconv from "iconv-lite";
+import { describe, expect, it } from "vitest";
+import { Ust } from "../../src/lib/Ust";
+
+const makeHeader = (encode: string): Array<string> => {
+  const headerLines = [
+    "[#VERSION]",
+    "UST Version1.2",
+    "[#SETTING]",
+    "Tempo=150.00",
+    "Tracks=1",
+    "Project=test",
+    "VoiceDir=%VOICE%{あああ}",
+    "OutFile=output.wav",
+    "CacheDir=main__.cache",
+    "Tool1=wavtool.exe",
+    "Tool2=resamp.exe",
+    "Flags=B50",
+    "Mode2=True",
+  ];
+  if (encode === "utf-8") {
+    return headerLines
+      .slice(0, 3)
+      .concat(["Charset=UTF-8"], headerLines.slice(3));
+  } else {
+    return headerLines;
+  }
+};
+
+describe("Ust", () => {
+  it("load_header", async () => {
+    const ustLines = makeHeader("shift-jis").concat([
+      "[#0000]",
+      "Length=1920",
+      "Lyric=あ",
+      "NoteNum=60",
+      "PreUtterance=",
+      "[#0001]",
+      "Length=1920",
+      "Lyric=あ",
+      "NoteNum=60",
+      "PreUtterance=",
+    ]);
+    const ustBuf = new File(
+      [iconv.encode(ustLines.join("\r\n"), "Windows-31j")],
+      "test.ust",
+      { type: "text/plane;charset=shift-jis" }
+    );
+    const u = new Ust();
+    expect(u.tempo).toBe(120);
+    expect(u.flags).toBeUndefined();
+    await u.Load(ustBuf);
+    expect(u.tempo).toBe(150);
+    expect(u.flags).toBe("B50");
+    expect(u.notes.length).toBe(2);
+    expect(u.notes[0].lyric).toBe("あ");
+  });
+  it("load_header_utf8", async () => {
+    const ustLines = makeHeader("utf-8").concat([
+      "[#0000]",
+      "Length=1920",
+      "Lyric=あ",
+      "NoteNum=60",
+      "PreUtterance=",
+      "[#0001]",
+      "Length=1920",
+      "Lyric=あ",
+      "NoteNum=60",
+      "PreUtterance=",
+    ]);
+    const ustBuf = new File(
+      [iconv.encode(ustLines.join("\r\n"), "utf-8")],
+      "test.ust",
+      { type: "text/plane;charset=utf-8" }
+    );
+    const u = new Ust();
+    expect(u.tempo).toBe(120);
+    expect(u.flags).toBeUndefined();
+    await u.Load(ustBuf);
+    expect(u.tempo).toBe(150);
+    expect(u.flags).toBe("B50");
+    expect(u.notes.length).toBe(2);
+    expect(u.notes[0].lyric).toBe("あ");
+  });
+  it("minimum_note", async () => {
+    const ustLines = makeHeader("utf-8").concat([
+      "[#0000]",
+      "Length=1920",
+      "Lyric=あ",
+      "NoteNum=60",
+      "PreUtterance=",
+    ]);
+    const ustBuf = new File(
+      [iconv.encode(ustLines.join("\r\n"), "utf-8")],
+      "test.ust",
+      { type: "text/plane;charset=utf-8" }
+    );
+    const u = new Ust();
+    await u.Load(ustBuf);
+    expect(u.notes.length).toBe(1);
+    expect(u.notes[0].index).toBe(0);
+    expect(u.notes[0].length).toBe(1920);
+    expect(u.notes[0].lyric).toBe("あ");
+    expect(u.notes[0].notenum).toBe(60);
+    expect(u.notes[0].tempo).toBe(150);
+    expect(u.notes[0].hasTempo).toBeFalsy();
+    expect(u.notes[0].preutter).toBeUndefined();
+  });
+  it("fully_note", async () => {
+    const ustLines = makeHeader("utf-8").concat([
+        "[#0000]",
+        "Length=1920",
+        "Lyric=あ",
+        "NoteNum=60",
+        "Tempo=120",
+        "PreUtterance=1",
+        "@preuttr=2",
+        "VoiceOverlap=3",
+        "@overlap=4",
+        "StartPoint=5",
+        "@stpoint=6",
+        "@filename=filepath",
+        "@alias=あ_C4",
+        "Velocity=150",
+        "Intensity=80",
+        "Modulation=30",
+        "PitchBend=0,1,2,3",
+        "PBStart=-10.0",
+        "PBS=-5;3",
+        "PBY=1,2,3",
+        "PBW=10,20,30,40",
+        "PBM=,s,r,j,",
+        "Flags=g-5",
+        "VBR=1,2,3,4,5,6,7,8",
+        "Envelope=9,10,11,12,13,14,15,%,16,17,18",
+        "Label=aa",
+        "$direct=True",
+        "$region=1番",
+        "$region_end=イントロ",
+    ]);
+    const ustBuf = new File(
+      [iconv.encode(ustLines.join("\r\n"), "utf-8")],
+      "test.ust",
+      { type: "text/plane;charset=utf-8" }
+    );
+    const u = new Ust();
+    await u.Load(ustBuf);
+    expect(u.notes.length).toBe(1);
+    expect(u.notes[0].index).toBe(0);
+    expect(u.notes[0].length).toBe(1920);
+    expect(u.notes[0].lyric).toBe("あ");
+    expect(u.notes[0].notenum).toBe(60);
+    expect(u.notes[0].tempo).toBe(120);
+    expect(u.notes[0].hasTempo).toBeTruthy();
+    expect(u.notes[0].preutter).toBe(1);
+    expect(u.notes[0].overlap).toBe(3);
+    expect(u.notes[0].stp).toBe(5);
+    expect(u.notes[0].velocity).toBe(150);
+    expect(u.notes[0].intensity).toBe(80);
+    expect(u.notes[0].modulation).toBe(30);
+    expect(u.notes[0].pitches).toEqual([0,1,2,3]);
+    expect(u.notes[0].pbStart).toBe(-10);
+    expect(u.notes[0].pbs).toEqual({time:-5,height:3});
+    expect(u.notes[0].pby).toEqual([1,2,3]);
+    expect(u.notes[0].pbw).toEqual([10,20,30,40]);
+    expect(u.notes[0].pbm).toEqual(["","s","r","j",""]);
+    expect(u.notes[0].flags).toBe("g-5");
+    expect(u.notes[0].vibrato).toEqual({length:1,cycle:64,depth:5,fadeInTime:4,fadeOutTime:5,phase:6,height:7});
+    expect(u.notes[0].envelope).toEqual({point:[9,10,11,16,17],value:[12,13,14,15,18]});
+    expect(u.notes[0].label).toBe("aa");
+    expect(u.notes[0].direct).toBeTruthy();
+    expect(u.notes[0].region).toBe("1番");
+    expect(u.notes[0].regionEnd).toBe("イントロ");
+  });
+  it("tempo", async () => {
+    const ustLines = makeHeader("utf-8").concat([
+      "[#0000]",
+      "Length=1920",
+      "Lyric=あ",
+      "NoteNum=60",
+      "PreUtterance=",
+      "[#0001]",
+      "Length=1920",
+      "Lyric=あ",
+      "NoteNum=60",
+      "PreUtterance=",
+      "Tempo=120",
+      "[#0002]",
+      "Length=1920",
+      "Lyric=あ",
+      "NoteNum=60",
+      "PreUtterance=",
+    ]);
+    const ustBuf = new File(
+      [iconv.encode(ustLines.join("\r\n"), "utf-8")],
+      "test.ust",
+      { type: "text/plane;charset=utf-8" }
+    );
+    const u = new Ust();
+    await u.Load(ustBuf);
+    expect(u.notes.length).toBe(3);
+    expect(u.notes[0].tempo).toBe(150);
+    expect(u.notes[0].hasTempo).toBeFalsy();
+    expect(u.notes[1].tempo).toBe(120);
+    expect(u.notes[1].hasTempo).toBeTruthy();
+    expect(u.notes[2].tempo).toBe(120);
+    expect(u.notes[2].hasTempo).toBeFalsy();
+  });
+  it("pointer", async () => {
+    const ustLines = makeHeader("utf-8").concat([
+      "[#0000]",
+      "Length=1920",
+      "Lyric=あ",
+      "NoteNum=60",
+      "PreUtterance=",
+      "[#0001]",
+      "Length=1920",
+      "Lyric=あ",
+      "NoteNum=60",
+      "PreUtterance=",
+      "Tempo=120",
+      "[#0002]",
+      "Length=1920",
+      "Lyric=あ",
+      "NoteNum=60",
+      "PreUtterance=",
+    ]);
+    const ustBuf = new File(
+      [iconv.encode(ustLines.join("\r\n"), "utf-8")],
+      "test.ust",
+      { type: "text/plane;charset=utf-8" }
+    );
+    const u = new Ust();
+    await u.Load(ustBuf);
+    expect(u.notes.length).toBe(3);
+    expect(u.notes[0].prev).toBeUndefined();
+    expect(u.notes[0].next.index).toBe(1);
+    expect(u.notes[1].prev.index).toBe(0);
+    expect(u.notes[1].next.index).toBe(2);
+    expect(u.notes[2].prev.index).toBe(1);
+    expect(u.notes[2].next).toBeUndefined();
+  });
+});
---
+++ src/lib/Ust.ts
@@ -0,0 +1,144 @@
+/**
+ * UTAUのプロジェクトファイルであるustファイルを扱う。
+ * 基本的には本家UTAUの仕様に準拠するが、VoiceColorを使用するため拡張を行う。
+ */
+
+import { readTextFile } from "../services/readTextFile";
+import { Note } from "./Note";
+
+export class Ust {
+  /**プロジェクトのbpm */
+  private _tempo: number;
+  /** プロジェクトのフラグ */
+  flags: string;
+  /** ノート */
+  notes: Array<Note>;
+
+  /** bpm */
+  get tempo(): number {
+    return this._tempo;
+  }
+
+  /** bpm 10～512の浮動小数*/
+  set tempo(value: number) {
+    this._tempo = Math.max(Math.min(value, 512), 10);
+  }
+
+  /**
+   * UTAUのプロジェクトファイルであるustファイルを扱う。
+   * 基本的には本家UTAUの仕様に準拠するが、VoiceColorを使用するため拡張を行う。
+   */
+  constructor() {
+    this._tempo = 120;
+  }
+
+  /**
+   * プロジェクトを読み込む。
+   * @param buf プロジェクトのバイナリデータ
+   */
+  async Load(buf):Promise<void> {
+    let utfData: string;
+    return new Promise(async (resolve) => {
+      const data = await readTextFile(buf);
+      if (data.includes("Charset=UTF-8")) {
+        utfData = await readTextFile(buf, "UTF8");
+      }
+      const lines = (utfData === undefined ? data : utfData)
+        .replace(/\r/g, "")
+        .split("\n");
+      const noteStart = lines.indexOf("[#0000]");
+      this.LoadHeader(lines.slice(0, noteStart));
+      this.LoadNote(lines.slice(noteStart));
+      for (let i = 1; i < this.notes.length; i++) {
+        this.notes[i - 1].next = this.notes[i];
+        this.notes[i].prev = this.notes[i - 1];
+      }
+      resolve();
+    });
+  }
+
+  /**
+   * ustのヘッダ部分を読み込む。PC上のパスに当たる内容はすべて無視する。
+   * @param lines 1行毎に区切ったustヘッダ部分のデータ
+   */
+  private LoadHeader(lines: Array<string>) {
+    for (let i = 0; i < lines.length; i++) {
+      if (lines[i].startsWith("Tempo=")) {
+        this.tempo = parseFloat(lines[i].replace("Tempo=", ""));
+      } else if (lines[i].startsWith("Flags=")) {
+        this.flags = lines[i].replace("Flags=", "");
+      }
+    }
+  }
+
+  /**
+   * ustのノート部分を読み込む。`@`から始まるエントリは無視する。
+   * @param lines 1行毎に区切ったustノート部分のデータ
+   */
+  private LoadNote(lines: Array<string>) {
+    this.notes = new Array<Note>();
+    let note: Note | undefined;
+    let nowTempo: number = this.tempo;
+    lines.forEach((l) => {
+      if (l === "[#TRACKEND]") {
+        return;
+      } else if (l.startsWith("[#")) {
+        note = new Note();
+        note.index = parseInt(l.slice(2, -1));
+        note.tempo = nowTempo;
+        this.notes.push(note);
+      } else if (l.startsWith("Length=")) {
+        note.length = parseInt(l.replace("Length=", ""));
+      } else if (l.startsWith("Lyric=")) {
+        note.lyric = l.replace("Lyric=", "");
+      } else if (l.startsWith("NoteNum=")) {
+        note.notenum = parseInt(l.replace("NoteNum=", ""));
+      } else if (l.startsWith("Tempo=")) {
+        note.tempo = parseFloat(l.replace("Tempo=", ""));
+        nowTempo = note.tempo;
+        note.hasTempo = true;
+      } else if (l.startsWith("PreUtterance=")) {
+        const pre = parseFloat(l.replace("PreUtterance=", ""))
+        if(!isNaN(pre)){
+          note.preutter = pre;
+        }
+      } else if (l.startsWith("VoiceOverlap=")) {
+        note.overlap = parseFloat(l.replace("VoiceOverlap=", ""));
+      } else if (l.startsWith("StartPoint=")) {
+        note.stp = parseFloat(l.replace("StartPoint=", ""));
+      } else if (l.startsWith("Velocity=")) {
+        note.velocity = parseInt(l.replace("Velocity=", ""));
+      } else if (l.startsWith("Intensity=")) {
+        note.intensity = parseInt(l.replace("Intensity=", ""));
+      } else if (l.startsWith("Modulation=")) {
+        note.modulation = parseInt(l.replace("Modulation=", ""));
+      } else if (l.startsWith("PitchBend=")) {
+        note.pitches = l.replace("PitchBend=", "");
+      } else if (l.startsWith("PBStart=")) {
+        note.pbStart = parseFloat(l.replace("PBStart=", ""));
+      } else if (l.startsWith("PBS=")) {
+        note.pbs = l.replace("PBS=", "");
+      } else if (l.startsWith("PBY=")) {
+        note.pby = l.replace("PBY=", "");
+      } else if (l.startsWith("PBM=")) {
+        note.pbm = l.replace("PBM=", "");
+      } else if (l.startsWith("PBW=")) {
+        note.pbw = l.replace("PBW=", "");
+      } else if (l.startsWith("Flags=")) {
+        note.flags = l.replace("Flags=", "");
+      } else if (l.startsWith("VBR=")) {
+        note.vibrato = l.replace("VBR=", "");
+      } else if (l.startsWith("Envelope=")) {
+        note.envelope = l.replace("Envelope=", "");
+      } else if (l.startsWith("Label=")) {
+        note.label = l.replace("Label=", "");
+      } else if (l.startsWith("$direct=")) {
+        note.direct = l.replace("$direct=", "").toLowerCase() === "true";
+      } else if (l.startsWith("$region=")) {
+        note.region = l.replace("$region=", "");
+      } else if (l.startsWith("$region_end=")) {
+        note.regionEnd = l.replace("$region_end=", "");
+      }
+    });
+  }
+}
