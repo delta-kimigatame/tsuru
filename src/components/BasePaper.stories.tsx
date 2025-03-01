6a79cbfda12bba1cdca0d73b02c865602b14a08f
delta_kimigatame <sankaku.kuro@gmail.com>
2025-03-01 21:45:27 +0900

    BasePaperとそのstoryの作成

---
+++ src/components/BasePaper.stories.tsx
@@ -0,0 +1,90 @@
+import { createTheme, ThemeProvider } from "@mui/material/styles";
+import { Meta, StoryFn } from "@storybook/react";
+import React from "react";
+import { getDesignTokens } from "../config/theme";
+import { BasePaper, BasePaperProps } from "./BasePaper";
+
+export default {
+  title: "Components/BasePaper",
+  component: BasePaper,
+  argTypes: {},
+} as Meta;
+
+const lightTheme = createTheme(getDesignTokens("light"));
+const darkTheme = createTheme(getDesignTokens("dark"));
+
+const Template: StoryFn<BasePaperProps> = (args) => <BasePaper {...args} />;
+export const Default = Template.bind({});
+Default.args = {
+  title: "デフォルト",
+  children: (
+    <>
+      test
+      <br />
+      test
+    </>
+  ),
+};
+
+export const ElevationChange = Template.bind({});
+ElevationChange.args = {
+  title: "エレベーション6",
+  children: (
+    <>
+      test
+      <br />
+      test
+    </>
+  ),
+  elevation: 6,
+};
+
+export const SxAdd = Template.bind({});
+SxAdd.args = {
+  title: "sx(maxWidth:300)",
+  children: (
+    <>
+      test
+      <br />
+      test
+    </>
+  ),
+  sx: { maxWidth: 300 },
+};
+
+export const ThemeProviderStory = Template.bind({});
+ThemeProviderStory.args = {
+  title: "Theme test(light)",
+  children: (
+    <>
+      test
+      <br />
+      test
+    </>
+  ),
+};
+ThemeProviderStory.decorators = [
+  (Story) => (
+    <ThemeProvider theme={lightTheme}>
+      <Story />
+    </ThemeProvider>
+  ),
+];
+export const ThemeProviderStoryDark = Template.bind({});
+ThemeProviderStoryDark.args = {
+  title: "Theme test(dark)",
+  children: (
+    <>
+      test
+      <br />
+      test
+    </>
+  ),
+};
+ThemeProviderStoryDark.decorators = [
+  (Story) => (
+    <ThemeProvider theme={darkTheme}>
+      <Story />
+    </ThemeProvider>
+  ),
+];
