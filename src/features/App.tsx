6dcd976111b506f0ee0c155c85665438801438ff
delta_kimigatame <sankaku.kuro@gmail.com>
2025-03-02 06:59:27 +0900

    i18nの初期化動作を追記

--- src/features/App.tsx
+++ src/features/App.tsx
@@ -1,21 +1,30 @@
 import { CssBaseline } from "@mui/material";
 import { createTheme, ThemeProvider } from "@mui/material/styles";
 import * as React from "react";
+import { BasePaper } from "../components/common/BasePaper";
+import { Footer } from "../components/Footer/Footer";
 import { getDesignTokens } from "../config/theme";
 import { useInitializeApp } from "../hooks/useInitializeApp";
 import { useThemeMode } from "../hooks/useThemeMode";
+import i18n from "../i18n/configs";
+import { useCookieStore } from "../store/cookieStore";
 
 export const App: React.FC = () => {
   useInitializeApp();
   const mode_ = useThemeMode();
+  const { language } = useCookieStore();
   const theme = React.useMemo(
     () => createTheme(getDesignTokens(mode_)),
     [mode_]
   );
+  React.useMemo(() => {
+    i18n.changeLanguage(language);
+  }, [language]);
   return (
     <ThemeProvider theme={theme}>
       <CssBaseline />
-      表示テスト用仮の文字列
+      <BasePaper title="aaa">表示テスト用仮の文字列</BasePaper>
+      <Footer />
     </ThemeProvider>
   );
 };
