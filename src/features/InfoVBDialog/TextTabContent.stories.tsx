26f249287cd2ae5e33eebd79d40f532c26880037
delta_kimigatame <sankaku.kuro@gmail.com>
2025-03-03 21:51:29 +0900

    TextTabContenの作成

---
+++ src/features/InfoVBDialog/TextTabContent.stories.tsx
@@ -0,0 +1,73 @@
+import { createTheme, ThemeProvider } from "@mui/material/styles";
+import { Meta, StoryFn } from "@storybook/react";
+import * as iconv from "iconv-lite";
+import JSZip from "jszip";
+import React from "react";
+import { getDesignTokens } from "../../config/theme";
+import { EncodingOption } from "../../utils/EncodingMapping";
+import { TextTabContent, TextTabContentProps } from "./TextTabContent";
+
+const lightTheme = createTheme(getDesignTokens("light"));
+
+export default {
+  title: "VbInfoDialog/TextTabContent",
+  component: TextTabContent,
+} as Meta;
+
+// サンプルテキスト
+const sampleText = "あいう\r\nえお";
+
+// Shift-JIS 用のテキストファイルを作成
+const zipShift = new JSZip();
+const shiftJisFile = new File(
+  [iconv.encode(sampleText, "Windows-31j")],
+  "test.txt",
+  { type: "text/plane;charset=shift-jis" }
+);
+zipShift.file("test.txt", shiftJisFile);
+const shiftJisTextFile = zipShift.files["test.txt"];
+
+// UTF-8 用のテキストファイルを作成
+const zipUtf8 = new JSZip();
+const utf8File = new File([sampleText], "test.txt", {
+  type: "text/plain;charset=utf-8",
+});
+zipUtf8.file("test.txt", utf8File);
+const utf8TextFile = zipUtf8.files["test.txt"];
+
+const Template: StoryFn<TextTabContentProps> = (args) => (
+  <ThemeProvider theme={lightTheme}>
+    <TextTabContent {...args} />
+  </ThemeProvider>
+);
+
+export const ShiftJIS = Template.bind({});
+ShiftJIS.storyName = "Shift-JIS テキストファイル";
+ShiftJIS.args = {
+  textFile: shiftJisTextFile,
+  encoding: EncodingOption.SHIFT_JIS,
+};
+
+export const UTF8 = Template.bind({});
+UTF8.storyName = "UTF-8 テキストファイル";
+UTF8.args = {
+  textFile: utf8TextFile,
+  encoding: EncodingOption.UTF8,
+};
+
+export const TextUndefined = Template.bind({});
+ShiftJIS.storyName = "テキストファイルがundefined";
+ShiftJIS.args = {
+  textFile: undefined,
+  encoding: EncodingOption.SHIFT_JIS,
+};
+
+// 遅延処理用のヘルパー関数
+const delay = (ms: number) => new Promise((resolve) => setTimeout(resolve, ms));
+
+// 読み込み処理に3秒の遅延を入れるオーバーライド関数
+const delayedReadTextFile = async (buffer: ArrayBuffer, encoding: string) => {
+  await delay(3000);
+  // サンプルテキストとして "あいう\r\nえお" を返す
+  return "あいう\r\nえお";
+};
---
+++ src/features/InfoVBDialog/TextTabContent.tsx
@@ -0,0 +1,91 @@
+import { Box, LinearProgress, Typography } from "@mui/material";
+import JSZip from "jszip";
+import * as React from "react";
+import { useTranslation } from "react-i18next";
+import { extractFileFromZip } from "../../services/extractFileFromZip";
+import { readTextFile } from "../../services/readTextFile";
+import { useSnackBarStore } from "../../store/snackBarStore";
+import {
+  EncodingOption,
+  getFileReaderEncoding,
+} from "../../utils/EncodingMapping";
+
+/**
+ * このコンポーネントはInfoVBDialog内において、音源zip内に含まれるテキストファイルを表示するタブ要素の各タブの中身を扱う。
+ * @param props
+ * @returns
+ */
+export const TextTabContent: React.FC<TextTabContentProps> = (props) => {
+  const { t } = useTranslation();
+  /**
+   * テキストファイルを行毎に分割したもの
+   */
+  const [lines, setLines] = React.useState<string[] | undefined>(undefined);
+
+  /** snackbarの操作 */
+  const snackBarStore = useSnackBarStore();
+  /**
+   * 与えられるテキストファイルもしくは文字コードが変更された際の処理
+   */
+  React.useEffect(() => {
+    let isCancelled = false;
+    if (!props.textFile) {
+      setLines([]);
+      return;
+    }
+    /**
+     * テキストファイルを読み込むための非同期処理
+     */
+    const fetchLines = async () => {
+      try {
+        const buf = await extractFileFromZip(props.textFile);
+        const text = await readTextFile(
+          buf,
+          getFileReaderEncoding(props.encoding)
+        );
+        const newLines = text.replace(/\r\n/g, "\n").split("\n");
+        if (!isCancelled) {
+          setLines(newLines);
+        }
+      } catch {
+        if (!isCancelled) {
+          setLines([]);
+          snackBarStore.setSeverity("error");
+          snackBarStore.setValue(t("infoVBDialog.TextTabContent.error"));
+          snackBarStore.setOpen(true);
+        }
+      }
+    };
+    fetchLines();
+    /**
+     * componentがアンマウントされた際の処理
+     */
+    return () => {
+      isCancelled = true;
+    };
+  }, [props.textFile, props.encoding]);
+
+  return (
+    <Box sx={{ m: 1 }}>
+      {lines === undefined ? (
+        <LinearProgress />
+      ) : (
+        <Typography variant="body2" data-testid="text-content">
+          {lines.map((l, i) => (
+            <React.Fragment key={i}>
+              {l}
+              <br />
+            </React.Fragment>
+          ))}
+        </Typography>
+      )}
+    </Box>
+  );
+};
+
+export interface TextTabContentProps {
+  /** このコンポーネントで表示するzip内のtextファイル */
+  textFile: JSZip.JSZipObject;
+  /** テキストファイルを読み込むための文字コード */
+  encoding: EncodingOption;
+}
