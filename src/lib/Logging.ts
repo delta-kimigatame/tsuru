ad4f4d70a453d705f575c749c336f2aec2b34bae
delta_kimigatame <sankaku.kuro@gmail.com>
2025-03-15 08:53:18 +0900

    Loggingの作成

--- src/lib/Logging.ts
+++ src/lib/Logging.ts
@@ -1,11 +1,73 @@
+enum LogLevel {
+  DEBUG = "DEBUG",
+  INFO = "INFO",
+  WARN = "WARN",
+  ERROR = "ERROR",
+}
+/**
+ * プロジェクト全体のログ管理
+ */
 class Logging {
+  /** ログのデータの蓄積 */
   datas: Array<string>;
   constructor() {
     this.datas = new Array();
   }
-  log(value: string, source: string):void {
-    this.datas.push(new Date().toJSON() + "\t" + source + "\t" + value);
+  /**
+   * ログを成型して蓄積する
+   * @param level ログレベル
+   * @param value ログメッセージ
+   * @param source ログ送出元となったコンポーネントやクラス
+   */
+  private log(level: LogLevel, value: any, source: string): void {
+    const message =
+      typeof value === "object" ? JSON.stringify(value) : value.toString();
+    const entry = `${new Date().toISOString()}\t${source}\t${level}\t${message}`;
+    this.datas.push(entry);
   }
+
+  /**
+   * デバッグログを蓄積する
+   * @param value ログメッセージ
+   * @param source ログ送出元となったコンポーネントやクラス
+   */
+  debug = (value: any, source: string): void => {
+    this.log(LogLevel.DEBUG, value, source);
+  };
+
+  /**
+   * インフォログを蓄積する
+   * @param value ログメッセージ
+   * @param source ログ送出元となったコンポーネントやクラス
+   */
+  info = (value: any, source: string): void => {
+    this.log(LogLevel.INFO, value, source);
+  };
+
+  /**
+   * 警告ログを蓄積する
+   * @param value ログメッセージ
+   * @param source ログ送出元となったコンポーネントやクラス
+   */
+  warn = (value: any, source: string): void => {
+    this.log(LogLevel.WARN, value, source);
+  };
+
+  /**
+   * エラーログを蓄積する
+   * @param value ログメッセージ
+   * @param source ログ送出元となったコンポーネントやクラス
+   */
+  error = (value: any, source: string): void => {
+    this.log(LogLevel.ERROR, value, source);
+  };
+
+  /**
+   * 蓄積したログを削除する
+   */
+  clear = () => {
+    this.datas = new Array();
+  };
 }
-  
+
 export const LOG = new Logging();
