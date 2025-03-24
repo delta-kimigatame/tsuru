50bce8ac85a05a685333fa889af0e8e47ca81d25
delta_kimigatame <sankaku.kuro@gmail.com>
2025-03-24 21:02:30 +0900

    rangeメソッドの追加

--- __tests__/utils/array.test.ts
+++ __tests__/utils/array.test.ts
@@ -1,5 +1,5 @@
 import { describe, expect, it } from "vitest";
-import { last } from "../../src/utils/array";
+import { last, range } from "../../src/utils/array";
 
 describe("array", () => {
   it("last", () => {
@@ -7,5 +7,10 @@ describe("array", () => {
     const numberArray = [1, 2, 3];
     expect(last(strArray)).toBe("c");
     expect(last(numberArray)).toBe(3);
+  });
+  it("range", () => {
+    expect(range(0, 5)).toEqual([0, 1, 2, 3, 4, 5]);
+    expect(range(5, 0)).toEqual([0, 1, 2, 3, 4, 5]);
+    expect(range(-3, 5)).toEqual([-3, -2, -1, 0, 1, 2, 3, 4, 5]);
   });
 });
--- src/utils/array.ts
+++ src/utils/array.ts
@@ -2,3 +2,12 @@
 export const last = <T>(arr: T[]): T | undefined => {
   return arr[arr.length - 1];
 };
+
+/**
+ * a～bの範囲の数値列を生成
+ */
+export const range = (a: number, b: number): number[] => {
+  const small = Math.min(a, b);
+  const large = Math.max(a, b);
+  return Array.from({ length: large - small + 1 }, (_, i) => small + i);
+};
