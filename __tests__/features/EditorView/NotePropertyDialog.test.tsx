d76dfe3adeee063f6869012e745efc7c675f3eee
delta_kimigatame <sankaku.kuro@gmail.com>
2025-03-30 09:32:41 +0900

    NotePropertyDialogの作成とテスティング

---
+++ __tests__/features/EditorView/NotePropertyDialog.test.tsx
@@ -0,0 +1,228 @@
+import "@testing-library/jest-dom";
+import { fireEvent, render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
+import React from "react";
+import { beforeEach, describe, expect, it, vi } from "vitest";
+import {
+  NoteEdit,
+  NotePropertyDialog,
+} from "../../../src/features/EditorView/NotePropertyDialog";
+import { Note } from "../../../src/lib/Note";
+import { undoManager } from "../../../src/lib/UndoManager";
+import { Ust } from "../../../src/lib/Ust";
+import { useMusicProjectStore } from "../../../src/store/musicProjectStore";
+
+describe("NotePropertyDialog", () => {
+  beforeEach(() => {
+    undoManager.clear();
+  });
+  it("NoteEdit:設定した通り値が変更される", () => {
+    const note = new Note();
+    note.index = 0;
+    note.length = 480;
+    note.lyric = "あ";
+    note.notenum = 60;
+    const resultNotes = NoteEdit(note, {
+      length: 240,
+      lyric: "い",
+      notenum: 61,
+      preutter: 1,
+      overlap: 2,
+      stp: 3,
+      intensity: 10,
+      velocity: 20,
+      modulation: 30,
+    });
+    expect(resultNotes.index).toBe(0);
+    expect(resultNotes.length).toBe(240);
+    expect(resultNotes.lyric).toBe("い");
+    expect(resultNotes.notenum).toBe(61);
+    expect(resultNotes.preutter).toBe(1);
+    expect(resultNotes.overlap).toBe(2);
+    expect(resultNotes.stp).toBe(3);
+    expect(resultNotes.intensity).toBe(10);
+    expect(resultNotes.velocity).toBe(20);
+    expect(resultNotes.modulation).toBe(30);
+    //元のノートは不変
+    expect(note.index).toBe(0);
+    expect(note.length).toBe(480);
+    expect(note.lyric).toBe("あ");
+    expect(note.notenum).toBe(60);
+    expect(note.preutter).toBe(undefined);
+    expect(note.overlap).toBe(undefined);
+    expect(note.stp).toBe(undefined);
+    expect(note.intensity).toBe(undefined);
+    expect(note.velocity).toBe(undefined);
+    expect(note.modulation).toBe(undefined);
+
+    const undoResult = undoManager.undo()[0];
+    const redoResult = undoManager.redo()[0];
+    expect(undoResult).toEqual(note);
+    expect(redoResult).toEqual(resultNotes);
+  });
+  it("空のノートを渡すと初期値が描画され、ノートを渡すとノートの値で更新される", () => {
+    const { rerender } = render(
+      <NotePropertyDialog open={true} note={undefined} handleClose={() => {}} />
+    );
+    const lyricField = screen.getByRole("textbox", {
+      name: /editor\.noteProperty\.lyric/i,
+    });
+    const lengthField = screen.getByRole("spinbutton", {
+      name: /editor\.noteProperty\.length/i,
+    });
+    const notenumSelect = screen.getByRole("combobox");
+    const preutterField = screen.getByRole("spinbutton", {
+      name: /editor\.noteProperty\.preutter/i,
+    });
+    const overlapField = screen.getByRole("spinbutton", {
+      name: /editor\.noteProperty\.overlap/i,
+    });
+    const stpField = screen.getByRole("spinbutton", {
+      name: /editor\.noteProperty\.stp/i,
+    });
+    const intensitySlider = screen.getByRole("slider", {
+      name: /editor\.noteProperty\.intensity/i,
+    });
+    const velocitySlider = screen.getByRole("slider", {
+      name: /editor\.noteProperty\.velocity/i,
+    });
+    const modulationSlider = screen.getByRole("slider", {
+      name: /editor\.noteProperty\.modulation/i,
+    });
+    //初期値の確認
+    expect(lyricField).toHaveValue("あ");
+    expect(lengthField).toHaveValue(480);
+    expect(notenumSelect).toHaveTextContent("C4");
+    expect(preutterField).toHaveValue(null);
+    expect(overlapField).toHaveValue(null);
+    expect(stpField).toHaveValue(null);
+    expect(intensitySlider).toHaveAttribute("aria-valuenow", "100");
+    expect(velocitySlider).toHaveAttribute("aria-valuenow", "100");
+    expect(modulationSlider).toHaveAttribute("aria-valuenow", "0");
+    //再描画用のノート
+    const n = new Note();
+    n.lyric = "い";
+    n.length = 240;
+    n.notenum = 61;
+    n.preutter = 1;
+    n.overlap = 2;
+    n.stp = 3;
+    n.intensity = 10;
+    n.velocity = 20;
+    n.modulation = 30;
+    //再描画用
+    rerender(
+      <NotePropertyDialog open={true} note={n} handleClose={() => {}} />
+    );
+    //再描画後値が変わっていることを確認する。
+    expect(lyricField).toHaveValue("い");
+    expect(lengthField).toHaveValue(240);
+    expect(notenumSelect).toHaveTextContent("C#4");
+    expect(preutterField).toHaveValue(1);
+    expect(overlapField).toHaveValue(2);
+    expect(stpField).toHaveValue(3);
+    expect(intensitySlider).toHaveAttribute("aria-valuenow", "10");
+    expect(velocitySlider).toHaveAttribute("aria-valuenow", "20");
+    expect(modulationSlider).toHaveAttribute("aria-valuenow", "30");
+  });
+  it("フォームの値変更し、実行ボタンをクリックすると変更が適用されダイアログが閉じる", async () => {
+    const n = new Note();
+    n.lyric = "い";
+    n.length = 240;
+    n.notenum = 61;
+    n.preutter = 1;
+    n.overlap = 2;
+    n.stp = 3;
+    n.intensity = 10;
+    n.velocity = 20;
+    n.modulation = 30;
+    const store = useMusicProjectStore.getState();
+    store.setUst({} as Ust);
+    store.setNotes([n]);
+    const dialogCloseSpy = vi.fn();
+    render(
+      <NotePropertyDialog open={true} note={n} handleClose={dialogCloseSpy} />
+    );
+    const user = userEvent.setup();
+    const lyricField = screen.getByRole("textbox", {
+      name: /editor\.noteProperty\.lyric/i,
+    });
+    const lengthField = screen.getByRole("spinbutton", {
+      name: /editor\.noteProperty\.length/i,
+    });
+    const notenumSelect = screen.getByRole("combobox");
+    const preutterField = screen.getByRole("spinbutton", {
+      name: /editor\.noteProperty\.preutter/i,
+    });
+    const overlapField = screen.getByRole("spinbutton", {
+      name: /editor\.noteProperty\.overlap/i,
+    });
+    const stpField = screen.getByRole("spinbutton", {
+      name: /editor\.noteProperty\.stp/i,
+    });
+    const intensitySlider = screen.getByRole("slider", {
+      name: /editor\.noteProperty\.intensity/i,
+    });
+    const velocitySlider = screen.getByRole("slider", {
+      name: /editor\.noteProperty\.velocity/i,
+    });
+    const modulationSlider = screen.getByRole("slider", {
+      name: /editor\.noteProperty\.modulation/i,
+    });
+    //編集前の値の確認
+    expect(lyricField).toHaveValue("い");
+    expect(lengthField).toHaveValue(240);
+    expect(notenumSelect).toHaveTextContent("C#4");
+    expect(preutterField).toHaveValue(1);
+    expect(overlapField).toHaveValue(2);
+    expect(stpField).toHaveValue(3);
+    expect(intensitySlider).toHaveAttribute("aria-valuenow", "10");
+    expect(velocitySlider).toHaveAttribute("aria-valuenow", "20");
+    expect(modulationSlider).toHaveAttribute("aria-valuenow", "30");
+    //各フォームの編集操作
+    await user.clear(lyricField);
+    await user.type(lyricField, "う");
+    await user.clear(lengthField);
+    await user.type(lengthField, "480");
+    await user.click(notenumSelect);
+    const option1 = await screen.findByRole("option", { name: /C4/i });
+    await user.click(option1);
+    await user.clear(preutterField);
+    await user.type(preutterField, "5.1");
+    await user.clear(overlapField);
+    await user.type(overlapField, "-3.2");
+    await user.clear(stpField);
+    await user.type(stpField, "11");
+    fireEvent.change(intensitySlider, { target: { value: "40" } });
+    fireEvent.change(velocitySlider, { target: { value: "50" } });
+    fireEvent.change(modulationSlider, { target: { value: "60" } });
+    // 編集後の値の確認
+    expect(lyricField).toHaveValue("う");
+    expect(lengthField).toHaveValue(480);
+    expect(notenumSelect).toHaveTextContent("C4");
+    expect(preutterField).toHaveValue(5.1);
+    expect(overlapField).toHaveValue(-3.2);
+    expect(stpField).toHaveValue(11);
+    expect(intensitySlider).toHaveAttribute("aria-valuenow", "40");
+    expect(velocitySlider).toHaveAttribute("aria-valuenow", "50");
+    expect(modulationSlider).toHaveAttribute("aria-valuenow", "60");
+    //処理の実行
+    const button = screen.getByRole("button", {
+      name: /editor.noteProperty.submitButton/i,
+    });
+    fireEvent.click(button);
+    //ダイアログが閉じているはず
+    expect(dialogCloseSpy).toHaveBeenCalled();
+    // notesが更新されているはず
+    const resultNotes = useMusicProjectStore.getState().notes;
+    expect(resultNotes[0].lyric).toBe("う");
+    expect(resultNotes[0].length).toBe(480);
+    expect(resultNotes[0].notenum).toBe(60);
+    expect(resultNotes[0].preutter).toBe(5.1);
+    expect(resultNotes[0].overlap).toBe(-3.2);
+    expect(resultNotes[0].stp).toBe(11);
+    expect(resultNotes[0].intensity).toBe(40);
+    expect(resultNotes[0].velocity).toBe(50);
+    expect(resultNotes[0].modulation).toBe(60);
+  });
+});
--- src/features/EditorView/NotePropertyDialog.tsx
+++ src/features/EditorView/NotePropertyDialog.tsx
@@ -17,6 +17,8 @@ import {
 } from "@mui/material";
 import React from "react";
 import { useTranslation } from "react-i18next";
+import { EDITOR_CONFIG } from "../../config/editor";
+import { useWindowSize } from "../../hooks/useWindowSize";
 import { LOG } from "../../lib/Logging";
 import { Note } from "../../lib/Note";
 import { undoManager } from "../../lib/UndoManager";
@@ -40,8 +42,8 @@ export const NotePropertyDialog: React.FC<NotePropertyDialogProps> = (
   props
 ) => {
   const { t } = useTranslation();
-  const { vb, setNote } = useMusicProjectStore();
-
+  const { setNote } = useMusicProjectStore();
+  const windowSize = useWindowSize();
   const formReducer = (state: NoteState, action: NoteFormAction) => {
     switch (action.type) {
       case "UPDATE_FIELD":
@@ -53,6 +55,7 @@ export const NotePropertyDialog: React.FC<NotePropertyDialogProps> = (
     }
   };
   const [formState, dispatch] = React.useReducer(formReducer, initialFormData);
+  LOG.debug(`ノートの変更検知、編集内容の初期化`, "NotePropertyDialog");
   React.useEffect(() => {
     if (props.note) {
       dispatch({ type: "RESET", initial: pickNoteFields(props.note) });
@@ -71,66 +74,21 @@ export const NotePropertyDialog: React.FC<NotePropertyDialogProps> = (
       }、編集後パラメータ:${JSON.stringify(formState)}`,
       "NotePropertyDialog"
     );
-    const oldNote = props.note.deepCopy();
-    undoManager.register({
-      undo: undo,
-      undoArgs: oldNote,
-      redo: redo,
-      redoArgs: { n: oldNote, state: { ...formState } },
-      summary: `ノート編集。index:${
-        props.note.index
-      }、編集後パラメータ:${JSON.stringify(formState)}`,
-    });
-    const n = apply(props.note.deepCopy(), formState);
+    const n = NoteEdit(props.note.deepCopy(), formState);
     setNote(n.index, n);
     props.handleClose();
   };
 
-  /**
-   * undo用のコマンド。undoManagerはNote[]を返すことを期待するので、元のノートを配列にして返す
-   * @param oldNote 元のノート
-   * @returns 元のノートを長さ1の配列にしたもの
-   */
-  const undo = (oldNote: Note): Note[] => {
-    return [oldNote];
-  };
-
-  /**
-   * redo用のコマンド。undoManagerはNote[]を返すことを期待するので、元のノートを配列にして返す
-   * @param oldNote 元のノート
-   * @param state 編集後の値
-   * @returns 編集後のノートを長さ1の配列にしたもの
-   */
-  const redo = (args: { n: Note; state: NoteState }): Note[] => {
-    return [apply(args.n, args.state)];
-  };
-
-  /**
-   * 編集処理をノートに適用する
-   * @param n 適用先のノート
-   * @param state 編集値
-   * @returns 適用後のノート
-   */
-  const apply = (n: Note, state: NoteState): Note => {
-    n.lyric = state.lyric;
-    n.length = state.length;
-    n.notenum = state.notenum;
-    if (n.lyric !== "R") {
-      n.preutter = state.preutter;
-      n.overlap = state.overlap;
-      n.stp = state.stp;
-      n.velocity = state.velocity;
-      n.intensity = state.intensity;
-      n.modulation = state.modulation;
-    }
-    return n;
-  };
-
   return (
     <Dialog
-      open={props.open && vb !== null}
+      open={props.open}
       onClose={props.handleClose}
-      fullScreen
+      fullScreen={
+        windowSize.width < EDITOR_CONFIG.FULLSCREEN_DIALOG_THRESHOLD_WIDTH
+      }
+      fullWidth={
+        windowSize.width >= EDITOR_CONFIG.FULLSCREEN_DIALOG_THRESHOLD_WIDTH
+      }
     >
       <DialogTitle>
         <IconButton
@@ -146,126 +104,142 @@ export const NotePropertyDialog: React.FC<NotePropertyDialogProps> = (
         </IconButton>
       </DialogTitle>
       <DialogContent>
-        <TextField
-          fullWidth
-          variant="outlined"
+        <Box
           sx={{
-            m: 1,
+            display: "flex",
+            justifyContent: "space-between",
           }}
-          type="text"
-          label={t("editor.noteProperty.lyric")}
-          value={formState.lyric}
-          onChange={(e) =>
-            dispatch({
-              type: "UPDATE_FIELD",
-              key: "lyric",
-              value: e.target.value,
-            })
-          }
-        />
-        <TextField
-          fullWidth
-          variant="outlined"
-          sx={{
-            m: 1,
-          }}
-          type="number"
-          label={t("editor.noteProperty.length")}
-          value={formState.length}
-          onChange={(e) =>
-            dispatch({
-              type: "UPDATE_FIELD",
-              key: "length",
-              value:
-                e.target.value === ""
-                  ? 0
-                  : Math.max(parseInt(e.target.value), 0),
-            })
-          }
-        />
-        <FormControl fullWidth sx={{ m: 1 }}>
-          <InputLabel>{t("editor.noteProperty.notenum")}</InputLabel>
-          <Select
-            label={t("editor.noteProperty.notenum")}
-            variant="filled"
-            defaultValue=""
-            value={formState.notenum.toString()}
+        >
+          <TextField
+            fullWidth
+            variant="outlined"
+            sx={{
+              m: 1,
+            }}
+            type="text"
+            label={t("editor.noteProperty.lyric")}
+            data-testid="propertyLyric"
+            value={formState.lyric}
             onChange={(e) =>
               dispatch({
                 type: "UPDATE_FIELD",
-                key: "notenum",
-                value: parseInt(e.target.value),
+                key: "lyric",
+                value: e.target.value,
               })
             }
-            data-testid="notenum-select"
-          >
-            {range(24, 107).map((num) => (
-              <MenuItem value={num.toString()}>{noteNumToTone(num)}</MenuItem>
-            ))}
-          </Select>
-        </FormControl>
+          />
+          <TextField
+            fullWidth
+            variant="outlined"
+            sx={{
+              m: 1,
+            }}
+            type="number"
+            label={t("editor.noteProperty.length")}
+            data-testid="propertyLength"
+            value={formState.length}
+            onChange={(e) =>
+              dispatch({
+                type: "UPDATE_FIELD",
+                key: "length",
+                value:
+                  e.target.value === ""
+                    ? 0
+                    : Math.max(parseInt(e.target.value), 0),
+              })
+            }
+          />
+          <FormControl fullWidth sx={{ m: 1 }}>
+            <InputLabel>{t("editor.noteProperty.notenum")}</InputLabel>
+            <Select
+              label={t("editor.noteProperty.notenum")}
+              variant="filled"
+              defaultValue=""
+              value={formState.notenum.toString()}
+              onChange={(e) =>
+                dispatch({
+                  type: "UPDATE_FIELD",
+                  key: "notenum",
+                  value: parseInt(e.target.value),
+                })
+              }
+              data-testid="propertyNotenumSelect"
+            >
+              {range(24, 107).map((num) => (
+                <MenuItem value={num.toString()}>{noteNumToTone(num)}</MenuItem>
+              ))}
+            </Select>
+          </FormControl>
+        </Box>
         {formState.lyric !== "R" && (
           <>
-            <TextField
-              fullWidth
-              variant="outlined"
+            <Box
               sx={{
-                m: 1,
+                display: "flex",
+                justifyContent: "space-between",
               }}
-              type="number"
-              label={t("editor.noteProperty.preutter")}
-              value={formState.preutter}
-              onChange={(e) =>
-                dispatch({
-                  type: "UPDATE_FIELD",
-                  key: "preutter",
-                  value:
-                    e.target.value === ""
-                      ? undefined
-                      : Math.max(parseFloat(e.target.value), 0),
-                })
-              }
-            />
-            <TextField
-              fullWidth
-              variant="outlined"
-              sx={{
-                m: 1,
-              }}
-              type="number"
-              label={t("editor.noteProperty.overlap")}
-              value={formState.overlap}
-              onChange={(e) =>
-                dispatch({
-                  type: "UPDATE_FIELD",
-                  key: "overlap",
-                  value:
-                    e.target.value === ""
-                      ? undefined
-                      : parseFloat(e.target.value),
-                })
-              }
-            />
-            <TextField
-              fullWidth
-              variant="outlined"
-              sx={{
-                m: 1,
-              }}
-              type="number"
-              label={t("editor.noteProperty.stp")}
-              value={formState.stp}
-              onChange={(e) =>
-                dispatch({
-                  type: "UPDATE_FIELD",
-                  key: "stp",
-                  value:
-                    e.target.value === ""
-                      ? undefined
-                      : Math.max(parseFloat(e.target.value), 0),
-                })
-              }
-            />
+            >
+              <TextField
+                fullWidth
+                variant="outlined"
+                sx={{
+                  m: 1,
+                }}
+                type="number"
+                label={t("editor.noteProperty.preutter")}
+                value={formState.preutter}
+                onChange={(e) =>
+                  dispatch({
+                    type: "UPDATE_FIELD",
+                    key: "preutter",
+                    value:
+                      e.target.value === ""
+                        ? undefined
+                        : Math.max(parseFloat(e.target.value), 0),
+                  })
+                }
+              />
+              <TextField
+                fullWidth
+                variant="outlined"
+                sx={{
+                  m: 1,
+                }}
+                type="number"
+                label={t("editor.noteProperty.overlap")}
+                value={formState.overlap}
+                onChange={(e) =>
+                  dispatch({
+                    type: "UPDATE_FIELD",
+                    key: "overlap",
+                    value:
+                      e.target.value === ""
+                        ? undefined
+                        : parseFloat(e.target.value),
+                  })
+                }
+              />
+              <TextField
+                fullWidth
+                variant="outlined"
+                sx={{
+                  m: 1,
+                }}
+                type="number"
+                label={t("editor.noteProperty.stp")}
+                value={formState.stp}
+                onChange={(e) =>
+                  dispatch({
+                    type: "UPDATE_FIELD",
+                    key: "stp",
+                    value:
+                      e.target.value === ""
+                        ? undefined
+                        : Math.max(parseFloat(e.target.value), 0),
+                  })
+                }
+              />
+            </Box>
             <Box sx={{ m: 1 }}>
               <Typography variant="subtitle1" gutterBottom>
                 {t("editor.noteProperty.velocity")}
@@ -358,7 +332,7 @@ export const NotePropertyDialog: React.FC<NotePropertyDialogProps> = (
           size="large"
           sx={{ mx: 1 }}
         >
-          {t("notePropertyDialog.submitButton")}
+          {t("editor.noteProperty.submitButton")}
         </Button>
       </DialogActions>
     </Dialog>
@@ -407,3 +381,63 @@ const pickNoteFields = (note: Note): NoteState => ({
   intensity: note.intensity,
   modulation: note.modulation,
 });
+
+/**
+ * undo用のコマンド。undoManagerはNote[]を返すことを期待するので、元のノートを配列にして返す
+ * @param oldNote 元のノート
+ * @returns 元のノートを長さ1の配列にしたもの
+ */
+const undo = (oldNote: Note): Note[] => {
+  return [oldNote];
+};
+
+/**
+ * redo用のコマンド。undoManagerはNote[]を返すことを期待するので、元のノートを配列にして返す
+ * @param oldNote 元のノート
+ * @param state 編集後の値
+ * @returns 編集後のノートを長さ1の配列にしたもの
+ */
+const redo = (args: { n: Note; state: NoteState }): Note[] => {
+  return [NoteEditCore(args.n.deepCopy(), args.state)];
+};
+
+/**
+ * 編集処理をノートに適用する
+ * @param n 適用先のノート
+ * @param state 編集値
+ * @returns 適用後のノート
+ */
+const NoteEditCore = (n: Note, state: NoteState): Note => {
+  n.lyric = state.lyric;
+  n.length = state.length;
+  n.notenum = state.notenum;
+  if (n.lyric !== "R") {
+    n.preutter = state.preutter;
+    n.overlap = state.overlap;
+    n.stp = state.stp;
+    n.velocity = state.velocity;
+    n.intensity = state.intensity;
+    n.modulation = state.modulation;
+  }
+  return n;
+};
+
+/**
+ * 編集処理をノートに適用する
+ * @param n 適用先のノート
+ * @param state 編集値
+ * @returns 適用後のノート
+ */
+export const NoteEdit = (note: Note, state: NoteState): Note => {
+  const oldNote = note.deepCopy();
+  undoManager.register({
+    undo: undo,
+    undoArgs: oldNote,
+    redo: redo,
+    redoArgs: { n: oldNote, state: { ...state } },
+    summary: `ノート編集。index:${
+      note.index
+    }、編集後パラメータ:${JSON.stringify(state)}`,
+  });
+  return NoteEditCore(note.deepCopy(), state);
+};
