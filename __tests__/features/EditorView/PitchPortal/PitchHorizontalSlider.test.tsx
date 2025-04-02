de103d61f5a5ff68a8e927790f5f4f9e87502a5d
delta_kimigatame <sankaku.kuro@gmail.com>
2025-04-02 20:20:42 +0900

    PitchHorizontalSliderの作成とテスト

---
+++ __tests__/features/EditorView/PitchPortal/PitchHorizontalSlider.test.tsx
@@ -0,0 +1,185 @@
+import "@testing-library/jest-dom";
+import { fireEvent, render, screen } from "@testing-library/react";
+import React from "react";
+import { beforeEach, describe, expect, it, vi } from "vitest";
+import { PitchHorizontalSlider } from "../../../../src/features/EditorView/PitchPortal/PitchHorizontalSlider";
+import { Note } from "../../../../src/lib/Note";
+import { Ust } from "../../../../src/lib/Ust";
+import { useMusicProjectStore } from "../../../../src/store/musicProjectStore";
+
+describe("PitchHorizontalSlider", () => {
+  const createNote = (): Note => {
+    const n = new Note();
+    n.index = 0;
+    n.length = 480;
+    n.notenum = 60;
+    n.lyric = "あ";
+    n.hasTempo = false;
+    n.tempo = 120;
+    return n;
+  };
+  beforeEach(() => {
+    vi.restoreAllMocks();
+  });
+  it("PitchHorizontalSlider:targetIndexがundefinedの時表示されない", () => {
+    const n = createNote();
+    render(
+      <PitchHorizontalSlider
+        targetIndex={undefined}
+        note={n}
+        setHasUpdate={() => {}}
+      />
+    );
+    expect(screen.queryByTestId("pitchHorizontalSlider")).toBeNull();
+  });
+  it("PitchHorizontalSlider:noteがundefinedの時表示されない", () => {
+    const n = createNote();
+    render(
+      <PitchHorizontalSlider
+        targetIndex={undefined}
+        note={n}
+        setHasUpdate={() => {}}
+      />
+    );
+    expect(screen.queryByTestId("pitchHorizontalSlider")).toBeNull();
+  });
+  it("PitchHorizontalSlider:targetIndexが0かつ、note.prev===nullの時、minは0", () => {
+    const n = createNote();
+    n.pbs = "-40;20";
+    n.setPbw([100, 200]);
+    render(
+      <PitchHorizontalSlider targetIndex={0} note={n} setHasUpdate={() => {}} />
+    );
+    const slider = screen.getByRole("slider", {
+      name: /pitchHorizontalSlider/i,
+    });
+    expect(slider).toHaveAttribute("aria-valuemin", "0");
+    expect(slider).toHaveAttribute("aria-valuemax", "100");
+  });
+  it("PitchHorizontalSlider:targetIndexが0かつ、note.prevが定義されているとき、minは-prev.msLength", () => {
+    const n = createNote();
+    n.pbs = "-40;20";
+    n.setPbw([100, 200]);
+    const pn = createNote();
+    n.prev = pn;
+    render(
+      <PitchHorizontalSlider targetIndex={0} note={n} setHasUpdate={() => {}} />
+    );
+    const slider = screen.getByRole("slider", {
+      name: /pitchHorizontalSlider/i,
+    });
+    expect(slider).toHaveAttribute("aria-valuemin", "-500");
+    expect(slider).toHaveAttribute("aria-valuemax", "100");
+  });
+  it("PitchHorizontalSlider:targetIndex非0かつ非最後のとき、minは0でmaxは次のpbwと対象pbwの和", () => {
+    const n = createNote();
+    n.pbs = "-40;20";
+    n.setPbw([100, 200]);
+    render(
+      <PitchHorizontalSlider targetIndex={1} note={n} setHasUpdate={() => {}} />
+    );
+    const slider = screen.getByRole("slider", {
+      name: /pitchHorizontalSlider/i,
+    });
+    expect(slider).toHaveAttribute("aria-valuemin", "0");
+    expect(slider).toHaveAttribute("aria-valuemax", "300");
+  });
+  it("PitchHorizontalSlider:targetIndex非0かつ最後のとき、minは0でmaxは直前までのpbwとpbsTimeの和とn.msLengthの差", () => {
+    const n = createNote();
+    n.pbs = "-40;20";
+    n.setPbw([100, 200]);
+    render(
+      <PitchHorizontalSlider targetIndex={2} note={n} setHasUpdate={() => {}} />
+    );
+    const slider = screen.getByRole("slider", {
+      name: /pitchHorizontalSlider/i,
+    });
+    expect(slider).toHaveAttribute("aria-valuemin", "0");
+    //ノート長が500ms、pbsTimeが-40でpbw0が100のため、440が期待される
+    expect(slider).toHaveAttribute("aria-valuemax", "440");
+  });
+  it("PitchHorizontalSlider:targetIndexが0かつ、slider操作はpbsTimeとpbw[0]を更新する", () => {
+    const n = createNote();
+    n.pbs = "-40;20";
+    n.setPbw([100, 200]);
+    const pn = createNote();
+    n.prev = pn;
+    const store = useMusicProjectStore.getState();
+    store.setUst({} as Ust);
+    store.setNotes([pn, n]);
+    const setHasUpdateSpy = vi.fn();
+    render(
+      <PitchHorizontalSlider
+        targetIndex={0}
+        note={n}
+        setHasUpdate={setHasUpdateSpy}
+      />
+    );
+    const slider = screen.getByRole("slider", {
+      name: /pitchHorizontalSlider/i,
+    });
+    expect(slider).toHaveAttribute("aria-valuenow", "-40");
+    fireEvent.change(slider, { target: { value: "-60" } });
+    expect(setHasUpdateSpy).toHaveBeenCalledWith(true);
+    //notesが更新されているはず
+    const resultNote = useMusicProjectStore.getState().notes[1];
+    expect(resultNote.pbs.time).toBe(-60);
+    expect(resultNote.pbw).toEqual([120, 200]);
+  });
+  it("PitchHorizontalSlider:targetIndexが非0かつ非最後の場合、slider操作はpbwと次のpbwを更新する", () => {
+    const n = createNote();
+    n.pbs = "-40;20";
+    n.setPbw([100, 200]);
+    const pn = createNote();
+    n.prev = pn;
+    const store = useMusicProjectStore.getState();
+    store.setUst({} as Ust);
+    store.setNotes([pn, n]);
+    const setHasUpdateSpy = vi.fn();
+    render(
+      <PitchHorizontalSlider
+        targetIndex={1}
+        note={n}
+        setHasUpdate={setHasUpdateSpy}
+      />
+    );
+    const slider = screen.getByRole("slider", {
+      name: /pitchHorizontalSlider/i,
+    });
+    expect(slider).toHaveAttribute("aria-valuenow", "100");
+    fireEvent.change(slider, { target: { value: "120" } });
+    expect(setHasUpdateSpy).toHaveBeenCalledWith(true);
+    //notesが更新されているはず
+    const resultNote = useMusicProjectStore.getState().notes[1];
+    expect(resultNote.pbs.time).toBe(-40);
+    expect(resultNote.pbw).toEqual([120, 180]);
+  });
+  it("PitchHorizontalSlider:targetIndexが最後の場合、slider操作は最後のpbwを更新する", () => {
+    const n = createNote();
+    n.pbs = "-40;20";
+    n.setPbw([100, 200]);
+    const pn = createNote();
+    n.prev = pn;
+    const store = useMusicProjectStore.getState();
+    store.setUst({} as Ust);
+    store.setNotes([pn, n]);
+    const setHasUpdateSpy = vi.fn();
+    render(
+      <PitchHorizontalSlider
+        targetIndex={2}
+        note={n}
+        setHasUpdate={setHasUpdateSpy}
+      />
+    );
+    const slider = screen.getByRole("slider", {
+      name: /pitchHorizontalSlider/i,
+    });
+    expect(slider).toHaveAttribute("aria-valuenow", "200");
+    fireEvent.change(slider, { target: { value: "120" } });
+    expect(setHasUpdateSpy).toHaveBeenCalledWith(true);
+    //notesが更新されているはず
+    const resultNote = useMusicProjectStore.getState().notes[1];
+    expect(resultNote.pbs.time).toBe(-40);
+    expect(resultNote.pbw).toEqual([100, 120]);
+  });
+});
---
+++ src/features/EditorView/PitchPortal/PitchHorizontalSlider.tsx
@@ -0,0 +1,102 @@
+import { Box, Slider } from "@mui/material";
+import React from "react";
+import { EDITOR_CONFIG } from "../../../config/editor";
+import { PIANOROLL_CONFIG } from "../../../config/pianoroll";
+import { useVerticalFooterMenu } from "../../../hooks/useVerticalFooterMenu";
+import { Note } from "../../../lib/Note";
+import { useMusicProjectStore } from "../../../store/musicProjectStore";
+import { PitchPortalProps } from "./PitchPortal";
+
+export const PitchHorizontalSlider: React.FC<
+  PitchPortalProps & { setHasUpdate: (f: boolean) => void }
+> = (props) => {
+  const verticalMenu = useVerticalFooterMenu();
+  const { setNote } = useMusicProjectStore();
+  const handleChange = (e, newValue: number) => {
+    props.setHasUpdate(true);
+    const n = props.note.deepCopy();
+    if (props.targetIndex === 0) {
+      const dif = newValue - n.pbs.time;
+      n.pbsTime = newValue;
+      n.pbw[0] -= dif;
+    } else {
+      const dif = newValue - n.pbw[props.targetIndex - 1];
+      console.log("PITCH_DEBUG", n.pbs.time, dif, n.pbw);
+      n.pbw[props.targetIndex - 1] = newValue;
+      if (props.targetIndex !== n.pbw.length) {
+        n.pbw[props.targetIndex] -= dif;
+      }
+    }
+    setNote(n.index, n);
+  };
+
+  return (
+    <>
+      {isHorizontalVisible(props.targetIndex, props.note) && (
+        <Box
+          sx={{
+            position: "fixed",
+            bottom: !verticalMenu
+              ? EDITOR_CONFIG.FOOTER_MENU_SIZE + EDITOR_CONFIG.SLIDER_PADDING
+              : EDITOR_CONFIG.SLIDER_PADDING,
+            right: verticalMenu
+              ? EDITOR_CONFIG.FOOTER_MENU_SIZE +
+                EDITOR_CONFIG.SLIDER_PADDING * 2
+              : EDITOR_CONFIG.SLIDER_PADDING * 2,
+            left:
+              EDITOR_CONFIG.SLIDER_PADDING * 2 +
+              PIANOROLL_CONFIG.TONEMAP_WIDTH +
+              EDITOR_CONFIG.SLIDER_SIZE,
+            zIndex: 100,
+          }}
+        >
+          <Slider
+            color="error"
+            step={0.1}
+            min={horizontalMin(props.targetIndex, props.note)}
+            max={horizontalMax(props.targetIndex, props.note)}
+            aria-label="pitchHorizontalSlider"
+            data-testid="pitchHorizontalSlider"
+            value={
+              props.targetIndex === 0
+                ? props.note.pbs.time
+                : props.note.pbw[props.targetIndex - 1]
+            }
+            onChange={handleChange}
+          />
+        </Box>
+      )}
+    </>
+  );
+};
+
+const horizontalMin = (targetIndex: number, note: Note): number => {
+  if (targetIndex === 0) {
+    if (note.prev === null || note.prev === undefined) {
+      return 0;
+    } else {
+      return note.prev.msLength * -1;
+    }
+  } else {
+    return 0;
+  }
+};
+const horizontalMax = (targetIndex: number, note: Note): number => {
+  if (targetIndex === 0) {
+    //1点目
+    return note.pbw[0];
+  } else if (targetIndex === note.pbw.length) {
+    //最後
+    // 1つ手前のノートまでのpbwの累積
+    const total =
+      note.pbw.slice(0, targetIndex - 1).reduce((s, c) => s + c, 0) +
+      note.pbs.time;
+    return note.msLength - total;
+  } else {
+    return note.pbw[targetIndex - 1] + note.pbw[targetIndex];
+  }
+};
+
+const isHorizontalVisible = (targetIndex: number, note: Note): boolean => {
+  return note !== undefined && targetIndex !== undefined;
+};
