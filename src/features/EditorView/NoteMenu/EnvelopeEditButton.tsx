f70715217ffe108b81ca4c12d6166a000e0c3865
delta_kimigatame <sankaku.kuro@gmail.com>
2025-04-04 06:44:45 +0900

    EnvelopeEditButtonの作成とテスト

---
+++ src/features/EditorView/NoteMenu/EnvelopeEditButton.tsx
@@ -0,0 +1,27 @@
+import { IconButton } from "@mui/material";
+import React from "react";
+import { EnvelopeNoteIcon } from "../../../components/EditorView/NoteMenu/EnvelopeNoteIcon";
+import { Note } from "../../../lib/Note";
+import { useMusicProjectStore } from "../../../store/musicProjectStore";
+
+export const EnvelopeEditButton: React.FC<EnvelopeEditButtonProps> = (
+  props
+) => {
+  const { notes } = useMusicProjectStore();
+
+  return (
+    <IconButton
+      disabled={notes[props.selectedNotesIndex[0]].lyric === "R"}
+      data-testid="envelopeEditButton"
+      onClick={() => {
+        props.setEnvelopeTargetNote(notes[props.selectedNotesIndex[0]]);
+      }}
+    >
+      <EnvelopeNoteIcon />
+    </IconButton>
+  );
+};
+export interface EnvelopeEditButtonProps {
+  selectedNotesIndex: Array<number>;
+  setEnvelopeTargetNote: (n: Note | undefined) => void;
+}
