88cc00b52f45fc299e306b610f747c1b8c4e6e19
delta_kimigatame <sankaku.kuro@gmail.com>
2025-04-12 07:08:08 +0900

    初回のみログ出力するよう変更

--- src/hooks/useInitializeApp.ts
+++ src/hooks/useInitializeApp.ts
@@ -1,3 +1,4 @@
+import React from "react";
 import { LOG } from "../lib/Logging";
 import { useInitializeCookieStore } from "../store/cookieStore";
 
@@ -5,11 +6,15 @@ import { useInitializeCookieStore } from "../store/cookieStore";
  * Appsを初期化する際に実行するカスタムフック
  */
 export const useInitializeApp = (): void => {
-  LOG.info("アプリケーションの初期化", "useInitializeApp");
-  LOG.debug(window.navigator.userAgent, "useInitializeApp");
-  LOG.debug(
-    "画面サイズ:" + [window.innerWidth, window.innerHeight],
-    "useInitializeApp"
-  );
+  const initialized = React.useRef(false);
+  if (!initialized.current) {
+    LOG.info("アプリケーションの初期化", "useInitializeApp");
+    LOG.debug(window.navigator.userAgent, "useInitializeApp");
+    LOG.debug(
+      "画面サイズ:" + [window.innerWidth, window.innerHeight],
+      "useInitializeApp"
+    );
+    initialized.current = true;
+  }
   useInitializeCookieStore();
 };
