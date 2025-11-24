2a40da6eb40d9ee3ca6ae47d7688a79ce2967fff
delta_kimigatame <sankaku.kuro@gmail.com>
2025-11-24 09:10:00 +0900

    歌詞変換動作がundo/redoできない不具合の修正

--- src/features/EditorView/LyricTextBox.tsx
+++ src/features/EditorView/LyricTextBox.tsx
@@ -1,6 +1,8 @@
 import { Menu, TextField } from "@mui/material";
 import React from "react";
 import { useTranslation } from "react-i18next";
+import { Note } from "../../lib/Note";
+import { undoManager } from "../../lib/UndoManager";
 import { useMusicProjectStore } from "../../store/musicProjectStore";
 
 export const LyricTextBox: React.FC<LyricTextBoxProps> = (props) => {
@@ -9,6 +11,7 @@ export const LyricTextBox: React.FC<LyricTextBoxProps> = (props) => {
   const [lyric, setLyric] = React.useState("");
 
   const handleMenuClose = () => {
+    RegistUndo(notes[props.targetNoteIndex], lyric);
     setNoteProperty(props.targetNoteIndex, "lyric", lyric);
     props.setTargetNoteIndex(undefined);
   };
@@ -54,3 +57,39 @@ export interface LyricTextBoxProps {
     y: number;
   } | null;
 }
+
+/**
+ * undo用のコマンド。undoManagerはNote[]を返すことを期待するので、元のノートを配列にして返す
+ * @param oldNote 元のノート
+ * @returns 元のノートを長さ1の配列にしたもの
+ */
+const undo = (oldNote: Note): Note[] => {
+  return [oldNote];
+};
+
+/**
+ * redo用のコマンド。undoManagerはNote[]を返すことを期待するので、元のノートを配列にして返す
+ * @param oldNote 元のノート
+ * @param state 編集後の値
+ * @returns 編集後のノートを長さ1の配列にしたもの
+ */
+const redo = (args: { n: Note; lyric: string }): Note[] => {
+  return [SetLyricCore(args.n.deepCopy(), args.lyric)];
+};
+
+const SetLyricCore = (n: Note, lyric: string): Note => {
+  n.lyric = lyric;
+  return n;
+};
+
+const RegistUndo = (note: Note, lyric: string) => {
+  const oldNote = note.deepCopy();
+  undoManager.register({
+    undo: undo,
+    undoArgs: oldNote,
+    redo: redo,
+    redoArgs: { n: oldNote, lyric: lyric },
+    summary: `ノート歌詞変更。index:${note.index}、編集後歌詞:${lyric}`,
+  });
+  return SetLyricCore(note.deepCopy(), lyric);
+};
