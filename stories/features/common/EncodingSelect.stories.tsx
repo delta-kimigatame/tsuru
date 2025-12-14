ddebfeeaed953d1d2147a923a5426ecf3eeae4c8
delta_kimigatame <sankaku.kuro@gmail.com>
2025-12-14 21:36:53 +0900

    commonのストーリー整理

--- stories/features/common/EncodingSelect.stories.tsx
+++ stories/features/common/EncodingSelect.stories.tsx
@@ -1,5 +1,6 @@
 ﻿import { Meta, StoryObj } from "@storybook/react";
 import { EncodingSelect } from "../../../src/features/common/EncodingSelect";
+import { EncodingOption } from "../../../src/utils/EncodingMapping";
 
 const meta: Meta<typeof EncodingSelect> = {
   title: "features/common/EncodingSelect",
@@ -13,7 +14,39 @@ type Story = StoryObj<typeof meta>;
 export const Default: Story = {
   args: {
     disabled: false,
-    value: "",
+    value: EncodingOption.UTF8,
+    setValue: () => {},
+  },
+};
+
+export const ShiftJIS: Story = {
+  args: {
+    disabled: false,
+    value: EncodingOption.SHIFT_JIS,
+    setValue: () => {},
+  },
+};
+
+export const GB18030: Story = {
+  args: {
+    disabled: false,
+    value: EncodingOption.GB18030,
+    setValue: () => {},
+  },
+};
+
+export const GBK: Story = {
+  args: {
+    disabled: false,
+    value: EncodingOption.GBK,
+    setValue: () => {},
+  },
+};
+
+export const BIG5: Story = {
+  args: {
+    disabled: false,
+    value: EncodingOption.BIG5,
     setValue: () => {},
   },
 };
@@ -21,7 +54,7 @@ export const Default: Story = {
 export const Disabled: Story = {
   args: {
     disabled: true,
-    value: "",
+    value: EncodingOption.UTF8,
     setValue: () => {},
   },
 };
