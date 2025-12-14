5d23fe448e45cc6cfe01e74b7932833047fe415b
delta_kimigatame <sankaku.kuro@gmail.com>
2025-12-14 21:56:36 +0900

    Loggingのストーリー修正

--- stories/features/Logging/ErrorPaper.stories.tsx
+++ stories/features/Logging/ErrorPaper.stories.tsx
@@ -39,33 +39,4 @@ const meta: Meta<typeof ErrorScenario> = {
 export default meta;
 type Story = StoryObj<typeof meta>;
 
-export const WithError: Story = {};
-
-const DownloadScenario = () => {
-  const testError = new Error("テストエラー");
-  testError.stack = "Error: テストエラー\n    at TestComponent (test file)";
-  return (
-    <ErrorBoundary
-      fallbackRender={({ error, resetErrorBoundary }) => (
-        <ErrorPaper error={error} resetErrorBoundary={resetErrorBoundary} />
-      )}
-    >
-      <ErrorTrigger />
-    </ErrorBoundary>
-  );
-};
-
-export const DownloadAction: Story = {
-  // TODO: Uncomment and migrate to @storybook/test
-  // play: async ({ canvasElement }) => {
-  //   const canvas = within(canvasElement);
-  //   const errorButton = await canvas.getByRole("button", {
-  //     name: /エラー発生/i,
-  //   });
-  //   await userEvent.click(errorButton);
-  //   const downloadButton = await canvas.findByRole("button", {
-  //     name: /ログをダウンロード/i,
-  //   });
-  //   await userEvent.click(downloadButton);
-  // },
-};
+export const Default: Story = {};
