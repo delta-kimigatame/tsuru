9453b6f9b23e58ab07f22f9eae36f0602f0b52f1
delta_kimigatame <sankaku.kuro@gmail.com>
2025-12-16 16:15:03 +0900

    PitchPortalに簡易ピッチ編集機能追加

--- src/features/EditorView/PitchPortal/PitchPortal.tsx
+++ src/features/EditorView/PitchPortal/PitchPortal.tsx
@@ -1,11 +1,28 @@
-import { Box } from "@mui/material";
+import { Box, ButtonGroup, IconButton } from "@mui/material";
 import Portal from "@mui/material/Portal";
 import React from "react";
+import { AbovePitchIcon } from "../../../components/EditorView/PitchPortal/AbovePitchIcon";
+import { AccentPitchIcon } from "../../../components/EditorView/PitchPortal/AccentPitchIcon";
+import { BelowPitchIcon } from "../../../components/EditorView/PitchPortal/BelowPitchIcon";
+import { FromAbovePitchIcon } from "../../../components/EditorView/PitchPortal/FromAbovePitchIcon";
+import { FromAccentPitchIcon } from "../../../components/EditorView/PitchPortal/FromAccentPitchIcon";
+import { FromBelowPitchIcon } from "../../../components/EditorView/PitchPortal/FromBelowPitchIcon";
+import { FromReservePitchIcon } from "../../../components/EditorView/PitchPortal/FromReservePitchIcon";
+import { ReservePitchIcon } from "../../../components/EditorView/PitchPortal/ReservePitchIcon";
 import { EDITOR_CONFIG } from "../../../config/editor";
+import { COLOR_PALLET } from "../../../config/pallet";
+import { useThemeMode } from "../../../hooks/useThemeMode";
 import { useVerticalFooterMenu } from "../../../hooks/useVerticalFooterMenu";
 import { Note } from "../../../lib/Note";
 import { undoManager } from "../../../lib/UndoManager";
+import { useCookieStore } from "../../../store/cookieStore";
 import { useMusicProjectStore } from "../../../store/musicProjectStore";
+import {
+  abovePitch,
+  accentPitch,
+  belowPitch,
+  reservePitch,
+} from "../../../utils/pitchPattern";
 import { PitchHorizontalSlider } from "./PitchHorizontalSlider";
 import { PitchVerticalSlider } from "./PitchVerticalSlider";
 import { PoltamentAddFab } from "./PoltamentAddFab";
@@ -14,7 +31,10 @@ import { RotateModeFab } from "./RotateModeFab";
 
 export const PitchPortal: React.FC<PitchPortalProps> = (props) => {
   const verticalMenu = useVerticalFooterMenu();
-  const { notes } = useMusicProjectStore();
+  const { notes, tone, isMinor, setNote } = useMusicProjectStore();
+  const { colorTheme } = useCookieStore();
+  const mode = useThemeMode();
+  const pitchColor = COLOR_PALLET[colorTheme][mode]["pitch"];
   const [hasUpdate, setHasUpdate] = React.useState<boolean>(false);
   const [initialUndoIndex, setInitialUndoIndex] = React.useState<
     number | undefined
@@ -22,6 +42,25 @@ export const PitchPortal: React.FC<PitchPortalProps> = (props) => {
   const [initialUndoNote, setInitialUndoNote] = React.useState<
     Note | undefined
   >(undefined);
+
+  // 前のノートが無いか休符かをチェック
+  const isFromPattern =
+    props.note?.prev === undefined || props.note?.prev.lyric === "R";
+
+  // ピッチパターン適用ハンドラ
+  const handleApplyPitchPattern = (
+    applyFn: (note: Note, tone: number, isMinor: boolean) => void
+  ) => {
+    if (props.note === undefined) return;
+
+    // Noteのディープコピーを作成してからピッチパターンを適用
+    const copiedNote = props.note.deepCopy();
+    applyFn(copiedNote, tone, isMinor);
+
+    // zustandのstoreを更新
+    setNote(props.note.index, copiedNote);
+    setHasUpdate(true);
+  };
   React.useEffect(() => {
     if (props.note === undefined) {
       //コンポーネントマウント時もしくはピッチ編集モードを抜けたとき
@@ -70,6 +109,72 @@ export const PitchPortal: React.FC<PitchPortalProps> = (props) => {
         <Portal>
           <Box data-testid="pitchPortal">
             <PitchHorizontalSlider {...props} setHasUpdate={setHasUpdate} />
+            {/* ピッチパターン適用ボタン群 */}
+            <Box
+              sx={{
+                position: "fixed",
+                bottom:
+                  (!verticalMenu ? EDITOR_CONFIG.FOOTER_MENU_SIZE : 0) +
+                  EDITOR_CONFIG.SLIDER_SIZE * 2 +
+                  16,
+                right: verticalMenu
+                  ? EDITOR_CONFIG.FOOTER_MENU_SIZE +
+                    EDITOR_CONFIG.SLIDER_PADDING * 2
+                  : EDITOR_CONFIG.SLIDER_PADDING * 2,
+                zIndex: 100,
+              }}
+            >
+              <ButtonGroup
+                orientation="horizontal"
+                variant="contained"
+                size="small"
+                sx={{
+                  backgroundColor: "background.paper",
+                  boxShadow: 3,
+                }}
+              >
+                <IconButton
+                  onClick={() => handleApplyPitchPattern(belowPitch)}
+                  size="small"
+                >
+                  {isFromPattern ? (
+                    <FromBelowPitchIcon sx={{ color: pitchColor }} />
+                  ) : (
+                    <BelowPitchIcon sx={{ color: pitchColor }} />
+                  )}
+                </IconButton>
+                <IconButton
+                  onClick={() => handleApplyPitchPattern(abovePitch)}
+                  size="small"
+                >
+                  {isFromPattern ? (
+                    <FromAbovePitchIcon sx={{ color: pitchColor }} />
+                  ) : (
+                    <AbovePitchIcon sx={{ color: pitchColor }} />
+                  )}
+                </IconButton>
+                <IconButton
+                  onClick={() => handleApplyPitchPattern(accentPitch)}
+                  size="small"
+                >
+                  {isFromPattern ? (
+                    <FromAccentPitchIcon sx={{ color: pitchColor }} />
+                  ) : (
+                    <AccentPitchIcon sx={{ color: pitchColor }} />
+                  )}
+                </IconButton>
+                <IconButton
+                  onClick={() => handleApplyPitchPattern(reservePitch)}
+                  size="small"
+                >
+                  {isFromPattern ? (
+                    <FromReservePitchIcon sx={{ color: pitchColor }} />
+                  ) : (
+                    <ReservePitchIcon sx={{ color: pitchColor }} />
+                  )}
+                </IconButton>
+              </ButtonGroup>
+            </Box>
             {props.targetIndex !== undefined && (
               <Box
                 sx={{
