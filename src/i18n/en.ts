69a50fb4c07ba44702d9586d5bf7286d0791d3b2
delta_kimigatame <sankaku.kuro@gmail.com>
2025-12-28 11:41:45 +0900

    更新ログの作成

--- src/i18n/en.ts
+++ src/i18n/en.ts
@@ -66,6 +66,7 @@ export const translationEn = {
       "The workers used on this site do not collect any privacy-related data.",
     history: "Release Note",
     changelog: [
+      "2025/12/28\r\n[Fix] Fixed an issue where scrolling would not be restored after pitch editing.\r\n[Fix] Fixed an issue where loading UST files without flags could result in flags being set to 'undefined', causing them to be recognized as the 'e' flag.",
       '2025/12/24\r\n[Fix] Fixed a bug where an error occurred when using the phonemizer "(Japanese) Automatic VCV(with CVVC)" if the VC oto entry existed but the CV oto entry did not.',
       "2025/12/18\r\n[Fix] Fixed an issue where undo/redo did not work correctly when changing an alias or note length from the note long-press menu",
       "2025/12/17\r\n[Added] Added pitch pattern feature",
